7c02f981ce6e779cdb0920e71a056f3f141edf07
archaurix <deckeroweni@gmail.com>
2015-12-02 17:37:27 -0700

    Refactored Edge class

--- TSP/Edge.cs
+++ TSP/Edge.cs
@@ -2,76 +2,39 @@
 {
     class Edge
     {
-        private int startCityIndex;
-        private int endCityIndex;
+
+        private const int PHEROMONE_WEIGHT = 5;
+        //These are implied
+        //private int startCityIndex;
+        //private int endCityIndex;
         private double cost;
-        private bool visited;
+        //private bool visited;
         private int pheromones;
 
-        public Edge(int startCityIndex, int endCityIndex, bool visited, double cost)
+        public Edge(double cost)
         {
-            this.startCityIndex = startCityIndex;
-            this.endCityIndex = endCityIndex;
-            this.visited = visited;
             this.cost = cost;
             this.pheromones = 0;
         }
 
-        public Edge() { }
-
-        //public void increaseProbability()
-        //{
-        //    //TODO: correctly increase probability
-        //}
-
-        public int GetStartCityIndex()
+        public double Cost
         {
-            return startCityIndex;
-        }
-        public void SetStartCityIndex(int temp)
-        {
-            startCityIndex = temp;
-        }
-        
-        public int GetEndCityIndex()
-        {
-            return endCityIndex;
-        }
-        public void SetEndCityIndex(int temp)
-        {
-            endCityIndex = temp;
+            get
+            {
+                return cost;
+            }
         }
 
-        public double GetCost()
+        public int Pheromones
         {
-            return cost;
+            get
+            {
+                return pheromones * PHEROMONE_WEIGHT;
+            }
+            set
+            {
+                pheromones = value;
+            }
         }
-        public void SetCost(double temp)
-        {
-            cost = temp;
-        }
-
-        public bool IsVisited()
-        {
-            return visited;
-        }
-        public void SetVisited(bool temp)
-        {
-            visited = temp;
-        }
-
-        public void AddPheromone()
-        {
-            pheromones++;
-        }
-        public void RemovePheromone()
-        {
-            pheromones--;
-        }
-        public void SetPheromones(int ph)
-        {
-            pheromones = ph;
-        }
-
     }
 }
